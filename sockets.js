a8d845e15f16015a82258551bd49a03b41362ed5
Shellharbour-Digital <hello@shellharbour.digital>
2021-05-31 22:23:12 +1000

    Rooms added which allow the server to host many games

--- sockets.js
+++ sockets.js
@@ -4,34 +4,45 @@ function listen(io) {
     const pongNamespace = io.of('/pong');
     // Only on the pong namespace the server repsonds the following
     pongNamespace.on('connection', (socket) => {
+
+        // Keep room in scope
+        let room;
+
         // Identify connected players
         console.log('a user connected:', socket.id);
     
         // Track players in the room
         socket.on('ready', () => {
-            console.log('Player ready:', socket.id);
+
+            // Room creation
+            room = 'room' + Math.floor(readyPlayerCount / 2);
+            socket.join(room);
+
+            console.log('Player ready:', socket.id, room);
     
             readyPlayerCount++;
     
             // When 2 players have connected. Start the game!
             if (readyPlayerCount % 2 === 0) {
-                pongNamespace.emit('startGame', socket.id);
+                pongNamespace.in(room).emit('startGame', socket.id);
             }
         });
     
         socket.on('paddleMove', (paddleData) => {
             // Send paddle position to the other player
-            socket.broadcast.emit('paddleMove', paddleData);
+            socket.to(room).emit('paddleMove', paddleData);
         });
     
         socket.on('ballMove', (ballData) => {
             // Send ball position and score to the other player
-            socket.broadcast.emit('ballMove', ballData)
+            socket.to(room).emit('ballMove', ballData)
         });
     
         // Detect disconnects
         socket.on('disconnect', (reason) => {
             console.log(`Client: ${socket.id} disconnected: ${reason}`);
+            // Leave the room
+            socket.leave(room);
         });
     });
 }
